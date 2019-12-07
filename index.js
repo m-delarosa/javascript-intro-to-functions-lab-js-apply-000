2d00c4112edc7acc08b809414dbfeb9d1590ac80
Michael De La Rosa <mdelarosa86@gmail.com>
2019-12-07 10:37:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function logWhisper(string) {
 
 function sayHiToGrandma(string) {
   if(string.toLowerCase())
-  return "I can\'t hear you!"
+  return "YES INDEED!"
 }
 
 function sayHiToGrandma(string) {
