78fc79a5d6381cfb4b53cc63019b55ba4be7a7fd
Michael De La Rosa <mdelarosa86@gmail.com>
2019-12-07 09:41:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,3 +11,19 @@ describe('whisper(string)', function() {
 function whisper(string) {
   return string.toLowerCase()
 }
+
+describe('logShout(string)', function() {
+  it('calls console.log() its one argument in all caps', function() {
+    const spy = expect.spyOn(console, 'log').andCallThrough()
+
+    logShout('hello')
+
+    expect(spy).toHaveBeenCalledWith('HELLO')
+
+    console.log.restore()
+  })
+})
+
+function logshout(string) {
+  console.log string.toUpperCase()
+}
